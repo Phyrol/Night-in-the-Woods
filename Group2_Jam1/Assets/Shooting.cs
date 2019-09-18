adb41630567c853abc33a30d48592e5fb2889747
Phyrol <nitehawkk123@gmail.com>
2019-09-17 22:22:49 -0700

    Raycast and Shooting
    Added shooting with raycast (hit-scan shooting)
    
    Added a gun and particle effect for bullet.

---
+++ Group2_Jam1/Assets/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [Header("Gun Settings")]
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    [Header("Bullet Impact Physics")]
+    public float impactForce = 30f;
+
+    public Camera playerCam;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        {
+            //spawn particle effect at raycast hit point. Angle it to perpendicular vector
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            //destroy clone after 1 second
+            Destroy(impactGO, 1f);
+        }
+    }
+}
