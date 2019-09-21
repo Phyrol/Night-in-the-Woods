0b31a54d122697f4d0d96f2f22a770b813bbe0f9
Phyrol <nitehawkk123@gmail.com>
2019-09-20 22:52:46 -0700

    Added key collision and door opening
    Player collides with key, key is set to not active, then door is set to not active.

---
+++ Group2_Jam1/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    public GameObject Key;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Key.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Group2_Jam1/Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Key"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
