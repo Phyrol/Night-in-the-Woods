ae20329f96930832fd6a9a0322555fa4816a38d6
Phyrol <nitehawkk123@gmail.com>
2019-09-22 13:01:13 -0700

    ai stuff

---
+++ Group2_Jam1/Assets/Scripts/Enemy/chaseFlying.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaseFlying : MonoBehaviour
+{
+    private Transform player;
+    static Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < 10)
+        {
+            Vector3 direction = player.position - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > 1.6)
+            {
+                this.transform.Translate(0, 0, 0.04f);
+                anim.SetBool("isFlying", true);
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isFlying", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isFlying", false);
+            anim.SetBool("isAttacking", false);
+        }
+    }
+}
--- Group2_Jam1/Assets/Scripts/Shooting.cs
+++ Group2_Jam1/Assets/Scripts/Shooting.cs
@@ -5,7 +5,6 @@ public class Shooting : MonoBehaviour
     [Header("Gun Settings")]
     public float damage = 10f;
     public float range = 100f;
-    public float fireRate = 15f;
 
     [Header("Bullet Impact Physics")]
     public float impactForce = 30f;
@@ -14,13 +13,13 @@ public class Shooting : MonoBehaviour
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
+    private float bulletSpeed = 2000f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if (Input.GetButtonDown("Fire1"))
         {
-            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -28,10 +27,15 @@ public class Shooting : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
         {
             //spawn particle effect at raycast hit point. Angle it to perpendicular vector
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            if(hit.collider.tag.Equals("Bat"))
+            {
+                hit.transform.SendMessage("damage", 50f);
+            }
 
             //destroy clone after 1 second
             Destroy(impactGO, 1f);
